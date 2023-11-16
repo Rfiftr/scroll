00febfd21a1047d9f81670f9b6fac0718eaec16c
Rfiftr <107876669+Rfiftr@users.noreply.github.com>
2023-11-16 17:12:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,81 @@
-body {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    background-image: url('https://www.yo-yoo.co.il/rekaim/images/uploads/a1341f.jpg');
-    background-size: cover;
-    
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const board = document.querySelector('.board');
+const boardPlus = document.querySelector('.board-plus');
+const boardMinus = document.querySelector('.board-minus');
+
+let num = 0;
+
+plus.addEventListener('click', plusNumber);
+minus.addEventListener('click', minusNumber);
+
+function plusNumber() {
+    num++;
+    board.textContent = num;
+    updateNumbers();
 }
 
-.container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-
+function minusNumber() {
+    if (num > 0) {
+        num--;
+        board.textContent = num;
+        updateNumbers();
+    }
 }
 
+function updateNumbers() {
+    boardPlus.textContent = num + 1;
 
-.plus, .minus {
-    width: 100px;
-    height: 50px;
-    background-color: rgb(80, 19, 19);
-    color: white;
-    border: none;
-    border-radius: 12px;
-    font-size: 1.5rem;
+    if (num > 0) {
+        boardMinus.textContent = num - 1;
+    } else {
+        boardMinus.textContent = 'ㅤ';
+    }
 }
 
-.board-plus, .board-minus {
-    opacity: 0.3;
-    font-size: 0.8rem;
+document.addEventListener('wheel', handleScroll);
+document.addEventListener('touchstart', handleTouchStart);
+document.addEventListener('touchmove', handleTouchMove);
+
+let touchStartY;
+
+function handleScroll(event) {
+    const isScrollOnBoard = event.target.closest('.board') === board;
+    if (isScrollOnBoard) {
+        const direction = event.deltaY > 0 ? 'down' : 'up';
+        if (direction === 'up') {
+            num++;
+            updateNumbers();
+        } else {
+            if (num > 0) {
+                num--;
+                updateNumbers();
+            }
+        }
+        board.textContent = num;
+        event.preventDefault();
+    }
 }
 
-.board {
-    font-size: 2rem;
-    margin: 0;
-    padding: 10px 20px;
+function handleTouchStart(event) {
+    touchStartY = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+    const touchMoveY = event.touches[0].clientY;
+    const direction = touchMoveY > touchStartY ? 'down' : 'up';
+
+    if (direction === 'up') {
+        num++;
+        updateNumbers();
+    } else {
+        if (num > 0) {
+            num--;
+            updateNumbers();
+        }
+    }
+
+    touchStartY = touchMoveY;
+
+    event.preventDefault();
 }
